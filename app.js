b66665263e5b4f492cd6768b9c5169975749149a
OubellilFouad <oubellilfouad4@gmail.com>
2022-12-23 13:22:44 +0100

    Added functions that create the books and append them to the main library container

--- app.js
+++ app.js
@@ -24,3 +24,91 @@ document.addEventListener('click', (event) => {
     form.classList.add('none');
   }
 });
+
+const checkRead = function (btn) {
+  btn.addEventListener('click', () => {
+    if (btn.classList.contains('read')) {
+      btn.classList.remove('read');
+      btn.classList.add('notRead');
+      btn.textContent = 'Not Read';
+    } else {
+      btn.classList.add('read');
+      btn.classList.remove('notRead');
+      btn.textContent = 'Read';
+    }
+  });
+};
+const Delete = function (btn, index, arr) {
+  btn.addEventListener('click', () => {
+    arr.splice(index, 1);
+    window[`bookCon${index}`].remove();
+  });
+};
+function Book(name, author, pages, isReadobj) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.isReadobj = isReadobj;
+}
+Book.prototype.create = function (arr) {
+  window[`bookName${arr.length - 1}`] = document.createElement('p');
+  window[`bookAuthor${arr.length - 1}`] = document.createElement('p');
+  window[`bookPages${arr.length - 1}`] = document.createElement('p');
+
+  window[`bookName${arr.length - 1}`].textContent = this.name;
+  window[`bookAuthor${arr.length - 1}`].textContent = this.author;
+  window[`bookPages${arr.length - 1}`].textContent = this.pages;
+};
+Book.prototype.isRead = function (arr) {
+  window[`readBtn${arr.length - 1}`] = document.createElement('button');
+  window[`readBtn${arr.length - 1}`].classList.add('readBtn');
+  window[`readBtn${arr.length - 1}`].setAttribute('id', `btn${parseInt(arr.length - 1, 10)}`);
+  if (this.isReadobj) {
+    window[`readBtn${arr.length - 1}`].textContent = 'Read';
+    window[`readBtn${arr.length - 1}`].classList.add('read');
+  } else {
+    window[`readBtn${arr.length - 1}`].textContent = 'Not Read';
+    window[`readBtn${arr.length - 1}`].classList.add('notRead');
+  }
+  checkRead(window[`readBtn${arr.length - 1}`]);
+};
+const append = function (arr) {
+  window[`bookCon${arr.length - 1}`] = document.createElement('div');
+  window[`bookCon${arr.length - 1}`].classList.add('bookCon');
+  window[`bookCon${arr.length - 1}`].append(window[`bookName${arr.length - 1}`]);
+  window[`bookCon${arr.length - 1}`].append(window[`bookAuthor${arr.length - 1}`]);
+  window[`bookCon${arr.length - 1}`].append(window[`bookPages${arr.length - 1}`]);
+  window[`bookCon${arr.length - 1}`].append(window[`readBtn${arr.length - 1}`]);
+  window[`bookCon${arr.length - 1}`].append(window[`remove${arr.length - 1}`]);
+  library.appendChild(window[`bookCon${arr.length - 1}`]);
+};
+const remove = function (arr, obj) {
+  window[`remove${arr.length - 1}`] = document.createElement('button');
+  window[`remove${arr.length - 1}`].textContent = 'Remove';
+  Delete(window[`remove${arr.length - 1}`], arr.indexOf(obj), arr);
+};
+const store = function (obj) {
+  libraryArr.push(obj);
+};
+const reset = function () {
+  nameInput.value = '';
+  authorInput.value = '';
+  pagesInput.value = '';
+  isReadInput.checked = false;
+};
+formBtn.addEventListener('click', (event) => {
+  i++;
+  event.preventDefault();
+  if (nameInput.value && authorInput.value && pagesInput.value) {
+    window[`libraryBook${i}`] = new Book(nameInput.value, authorInput.value, pagesInput.value, isReadInput.checked);
+    store(window[`libraryBook${i}`]);
+    libraryArr[libraryArr.length - 1].create(libraryArr);
+    libraryArr[libraryArr.length - 1].isRead(libraryArr);
+    remove(libraryArr, window[`libraryBook${i}`]);
+    append(libraryArr);
+    reset();
+    form.classList.add('none');
+  } else {
+    console.log('empty');
+  }
+});
